702513efbc568963b998805cb29f4c6e77718c57
judvelasco29 <201980205@psu.palawan.edu.ph>
2022-02-24 19:46:44 +0800

    inc directory done

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php include 'inc/header.php'; ?>
+
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Contacts</h1>
+        <p class="lead">Here in contact page, contains the information of the specific website about how the user or customer could contact them</p>
+        <p class="lead">
+          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+        </p>
+      </main>
+
+
+<?php include 'inc/footer.php'; ?>
+     
---
+++ feature.php
@@ -0,0 +1,13 @@
+<?php include 'inc/header.php'; ?>
+
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Your Features</h1>
+        <p class="lead">A feature is a distinctive trait or a special attraction. Feature can also mean to give special attention to something. The word feature has several other senses as a noun and a verb. A feature is a unique quality or characteristic that something has.</p>
+        <p class="lead">
+          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+        </p>
+      </main>
+
+
+<?php include 'inc/footer.php'; ?>
